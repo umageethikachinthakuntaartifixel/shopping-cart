ebf243c6ba6511667c86c96add8b204239bb2952
umageethikachinthakuntaartifixel <umageethika.chinthakunta@artifixel.com>
2026-05-12 13:17:04 +0530

    Delete shopping-cart-with-prop-drilling/shopping-cart directory

--- shopping-cart-with-prop-drilling/shopping-cart/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Vite configuration file
-// This tells Vite to use the React plugin so JSX works
-export default defineConfig({
-  plugins: [react()],
-})
